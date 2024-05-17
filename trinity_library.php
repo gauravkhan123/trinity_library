e96a1b90ebaec59ddf6aa40b06faade78babadd4
gauravkhan123 <gauravkhan123@outlook.com>
2024-05-17 20:56:22 +0530

    first commit

---
+++ trinity_library.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+ * Plugin Name:       Trinity Library
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       web-based book management system for Trinity library's librarians.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Gaurav Khandelwal
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       trinity-library
+ * Domain Path:       /languages
+ */
+
